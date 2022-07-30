9c8cc716f9e2e6e390e97dc903d5a2f32bca758f
Chase Simmonds <chasexsimmonds@gmail.com>
2022-07-30 00:27:04 +0000

    removed commented out code

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,8 +3,6 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
-// import Modal from 'react-bootstrap/Modal';
-// import Form from '../components/form';
 
 function Navboi() {
 
